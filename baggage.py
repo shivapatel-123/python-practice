3ec6edbb54e4247774b0993aa00202590c483189
shivapatel-123 <121080722+shivapatel-123@users.noreply.github.com>
2025-10-20 17:21:16 +0530

    ss

---
+++ baggage.py
@@ -0,0 +1,9 @@
+number_of_passengers=5
+number_of_baggage=2
+security_check=True
+for passenger_count in range(1, number_of_passengers+1):
+    for baggage_count in range(1,number_of_baggage+1):
+        if(security_check==True):
+            print("Security check of passenger:", passenger_count, "-- baggage:", baggage_count,"baggage cleared")
+        else:
+            print("Security check of passenger:", passenger_count, "-- baggage:", baggage_count,"baggage not cleared")
